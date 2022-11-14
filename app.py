f711055c5593633c8f3b1ea50b2cbe5d181caaf8
Lin Yudong <54970185+Campfireman0314@users.noreply.github.com>
2022-11-14 17:29:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ project = hopsworks.login()
 fs = project.get_feature_store()
 
 mr = project.get_model_registry()
-model = mr.get_model("titanic_modal_more_specs", version=1)
+model = mr.get_model("titanic_survival_modal", version=1)
 model_dir = model.download()
 model = joblib.load(model_dir + "/titanic_model.pkl")
 
@@ -26,22 +26,22 @@ def tb_titanic(pclass,sex,age,sibsp,parch,embarked,fare_per_customer,cabin):
     input_list.append(cabin)
     # 'res' is a list of predictions returned as the label.
     #global res
-    res = model.predict(np.asarray(input_list).reshape(1, -1))
-    return "This guy will"
+    res = model.predict(np.asarray(input_list).reshape(1, 8))
+    return ("This guy will"+(" survive. " if res[0]=="S" else " die. "))
     
 demo = gr.Interface(
     fn=tb_titanic,
     title="Titanic Predictive Analytics",
     description="Predict survivals. 0 for dead and 1 for survived. ",
     inputs=[
-        gr.inputs.Number(default=1.0, label="pclass"),
-        gr.inputs.Number(default=1.0, label="gender"),
+        gr.inputs.Number(default=1.0, label="pclass, "),
+        gr.inputs.Number(default=1.0, label="gender, 0 for male and 1 for female"),
         gr.inputs.Number(default=1.0, label="age"),
         gr.inputs.Number(default=1.0, label="sibsp"),
         gr.inputs.Number(default=1.0, label="parch"),
-        gr.inputs.Number(default=1.0, label="embarked"),
+        gr.inputs.Number(default=1.0, label="embarked, 1 for C, 2 for S, 3 for Q, and 0 for unknown"),
         gr.inputs.Number(default=1.0, label="fare_per_customer"),
-        gr.inputs.Number(default=1.0, label="cabin"),
+        gr.inputs.Number(default=1.0, label="cabin, 1 for the known and 0 for the unknown"),
     ],
     outputs=gr.Textbox()
     )
