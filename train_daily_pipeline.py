0b166274a4b74e408058f69a088cdb50456d3db2
Pony_Yuhan <89634392+ponyyuhan@users.noreply.github.com>
2022-11-15 17:33:56 +0100

    Update train_daily_pipeline.py

--- train_daily_pipeline.py
+++ train_daily_pipeline.py
@@ -7,28 +7,29 @@ if LOCAL == False:
    stub = modal.Stub()
    image = modal.Image.debian_slim().pip_install(["hopsworks==3.0.4"]) 
 
-   @stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
+   @stub.function(image=image, schedule=modal.Period(days=1), 
+                  secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
    def f():
        g()
 
 
-def generate_titanic(survived, pclass_num_max, pclass_num_min, sex_female, sex_male, 
-                    age_max, age_min, sibsp_max, sibsp_min,parch_max,parch_min,embarked_max,
-                     embarked_min,fare_per_customer_max,fare_per_customer_min,cabin_have,cabin_none):
+def generate_titanic(survived, pclass_num_min, pclass_num_max, sex_male, sex_female, 
+                    age_max, age_min, sibsp_min, sibsp_max,parch_min,parch_max,embarked_min,
+                     embarked_max,fare_per_customer_max,fare_per_customer_min,cabin_none,cabin_one):
     """
     Returns a single iris flower as a single row in a DataFrame
     """
     import pandas as pd
     import random
 
-    df = pd.DataFrame({ "pclass": [random.random_integers(pclass_num_max, pclass_num_min)],
-                       "sex": [random.random_integers(sex_female, sex_male)],
+    df = pd.DataFrame({ "pclass": [random.randint(pclass_num_min, pclass_num_max)],
+                       "sex": [random.randint(sex_male, sex_female)],
                        "age": [random.uniform(age_max, age_min)],
-                       "sibsp": [random.random_integers(sibsp_max, sibsp_min)],
-                       "parch":[random.random_integers(parch_max,parch_min)],
-                       "embarked":[random.random_integers(embarked_max,embarked_min)],
+                       "sibsp": [random.randint(sibsp_min, sibsp_max)],
+                       "parch":[random.randint(parch_min,parch_max)],
+                       "embarked":[random.randint(embarked_min,embarked_max)],
                        "fare_per_customer":[random.uniform(fare_per_customer_max,fare_per_customer_min)],
-                       "cabin":[random.random_integers(cabin_have,cabin_none)]
+                       "cabin":[random.randint(cabin_none,cabin_one)]
                       })
     df['survived'] = survived
     return df
@@ -41,8 +42,8 @@ def get_random_titanic():
     import pandas as pd
     import random
 
-    survived_df = generate_titanic("S", 3, 1, 1, 0, 30, 0, 4, 0,5,0,3,1,200,0,1,0)
-    deceased_df = generate_titanic("D", 3, 1, 1, 0, 80, 50, 4, 0,5,0,3,1,200,0,1,0)
+    survived_df = generate_titanic("S", 1, 4, 0, 2, 30, 0, 0, 5,0,6,1,4,0,200,0,2)
+    deceased_df = generate_titanic("D", 1, 4, 0, 2, 80, 50, 0, 5,0,6,1,4,0,200,0,2)
 
     pick_random = random.uniform(0,2)
     if pick_random >= 1:
